765fffb70ff656bb580ef56c49f82224518359b4
grandmasponge <tobyr2006@gmail.com>
2025-03-17 10:40:45 +0000

    added openssl tls support

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,8 @@
 const std = @import("std");
+const c = @cImport({
+    @cInclude("openssl/ssl.h");
+    @cInclude("openssl/err.h");
+});
 
 const Method = enum {
     POST,
@@ -131,10 +135,13 @@ const Http_Request = struct {
         };
     }
 
-    pub fn send(self: Http_Request, connection: std.net.Stream) !void {
-        var writer = connection.writer();
+    pub fn send(self: Http_Request, ssl: ?*c.struct_ssl_st, allocator: std.mem.Allocator) !void {
+        var buf = std.ArrayList(u8).init(allocator);
+        defer buf.deinit();
+        const writer = buf.writer();
+
         const method_string = self.method.toString();
-        const path = "/";
+        const path = "/grandmasponge";
         const version_string = switch (self.version) {
             .first => "HTTP/1.1",
             .second => "HTTP/2.0",
@@ -153,17 +160,19 @@ const Http_Request = struct {
             const value = self.body.?;
             try writer.writeAll(value);
         }
+
+        const slice = buf.items;
+        _ = c.SSL_write(ssl, slice.ptr, @intCast(slice.len));
     }
 
-    pub fn response(_: Http_Request, allocator: std.mem.Allocator, connection: std.net.Stream) !Http_Response {
+    pub fn response(_: Http_Request, allocator: std.mem.Allocator, ssl: ?*c.struct_ssl_st) !Http_Response {
         var buffer = std.ArrayList(u8).init(allocator);
         defer buffer.deinit();
 
-        var reader = connection.reader();
         var chunk: [1024]u8 = undefined;
 
         while (true) {
-            const bytes_read = try reader.read(chunk[0..]);
+            const bytes_read = c.SSL_read(ssl, &chunk, 1024);
             if (bytes_read == 0) {
                 break;
             }
@@ -178,29 +187,69 @@ const Http_Request = struct {
 
 const client = struct {};
 
+pub fn init_ssl() void {
+    _ = c.OPENSSL_init_ssl(0, null);
+}
+
+pub fn create_ssl_ctx() !?*c.struct_ssl_ctx_st {
+    const method = c.TLS_client_method();
+    const ctx = c.SSL_CTX_new(method);
+
+    if (ctx == null) {
+        std.debug.print("failed to initalize ssl context", .{});
+        return error.failed_ssl;
+    }
+
+    _ = c.SSL_CTX_set_min_proto_version(ctx, c.TLS1_2_VERSION);
+    _ = c.SSL_CTX_set_max_proto_version(ctx, c.TLS1_3_VERSION);
+
+    return ctx;
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    var headers = std.StringHashMap([]const u8).init(allocator);
-    try headers.put("Content-Type", "text/html");
+    const host = "github.com";
 
-    const stream = try std.net.tcpConnectToHost(allocator, "neal.fun", 80);
-    defer stream.close();
+    init_ssl();
+    const ctx = try create_ssl_ctx();
 
-    var http_req = try Http_Request.request_builder(allocator, "https://neal.fun", Method.GET, Http_version.first, headers, null);
-    try http_req.send(stream);
+    var sock = try std.net.tcpConnectToHost(allocator, host, 443);
+    defer sock.close();
 
-    var res = try http_req.response(allocator, stream);
-    std.debug.print("Http Version {s} status code {} status message {s}\n", .{ res.version, res.status, res.status_message });
+    const fd = sock.handle;
 
-    var iterator = res.headers.iterator();
-    while (iterator.next()) |entry| {
-        std.debug.print("Header {s} : Value {s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+    const ssl = c.SSL_new(ctx);
+
+    _ = c.SSL_set_fd(ssl, fd);
+
+    _ = c.SSL_set_tlsext_host_name(ssl, host);
+
+    if (c.SSL_connect(ssl) <= 0) {
+        std.debug.print("failed to connect with ssl \n", .{});
+        return;
     }
+    //create our http request
 
-    std.debug.print("{s}\r\n", .{res.body});
+    var headers = std.StringHashMap([]const u8).init(allocator);
+    defer headers.deinit();
 
-    headers.deinit();
-    res.headers.deinit();
+    const request = try Http_Request.request_builder(allocator, "https://github.com/grandmasponge", Method.GET, Http_version.first, headers, null);
+    try request.send(ssl, allocator);
+
+    std.time.sleep(2000000);
+
+    var response = try request.response(allocator, ssl);
+    defer response.headers.deinit();
+
+    std.debug.print("{s} {} {s}\n", .{ response.version, response.status, response.status_message });
+
+    var iterator = response.headers.iterator();
+    while (iterator.next()) |entry| {
+        std.debug.print("{s} {s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+    }
+    std.debug.print("{s}\n", .{response.body});
+
+    c.SSL_CTX_free(ctx);
 }
